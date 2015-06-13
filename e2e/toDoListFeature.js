dbe9281978316778edf03bfb74e94036206d9150
Rodney Cullen <rodcul@gmail.com>
2015-06-13 16:27:15 +0100

    Removed karma, can't get it working

--- e2e/toDoListFeature.js
+++ e2e/toDoListFeature.js
@@ -3,4 +3,16 @@ describe('To Do List', function(){
     browser.get('http://localhost:8000');
     expect(browser.getTitle()).toEqual('To Do List Challenge');
   });
+
+
+  it('finds profiles', function() {
+    browser.get('http://localhost:8000');
+    
+
+    element(by.model('todoList.todoText')).sendKeys('Install Travis');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('todo')).getText()).
+        toEqual('Install Travis');
+  });
 });
