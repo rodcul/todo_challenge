777cba9a6697c5644f97ecdd4309205be14984cd
Rodney Cullen <rodcul@gmail.com>
2015-06-13 19:24:08 +0100

    Filter by all/active/completed

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -3,6 +3,8 @@ todoApp.controller('ToDoListController', [function() {
 	var self = this;
 	self.todos = [];
 
+	self.filter =
+
 	self.addTodo = function(task) {
 		self.todos.unshift({text: task, done:false});
 		self.todoText = '';
